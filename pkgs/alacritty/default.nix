5c1ea9834d548c3ea7214aeccb26b1944c8e4379
Uwe Hermann <uh@uleenucks.de>
2021-05-27 18:55:23 +0200

    * MOD: switched from termite to alacritty in dwm
    Signed-off-by: Uwe Hermann <uh@uleenucks.de>

--- pkgs/alacritty/default.nix
+++ pkgs/alacritty/default.nix
@@ -75,17 +75,16 @@ rustPlatform.buildRustPackage rec {
   '';
 
   installPhase = ''
-      install -D extra/linux/Alacritty.desktop -t $out/share/applications/
-      install -D extra/logo/compat/alacritty-term.svg $out/share/icons/hicolor/scalable/apps/Alacritty.svg
+    install -D extra/linux/Alacritty.desktop -t $out/share/applications/
+    install -D extra/logo/compat/alacritty-term.svg $out/share/icons/hicolor/scalable/apps/Alacritty.svg
 
-      # patchelf generates an ELF that binutils' "strip" doesn't like:
-      #    strip: not enough room for program headers, try linking with -N
-      # As a workaround, strip manually before running patchelf.
-      strip -S $out/bin/alacritty
+    # patchelf generates an ELF that binutils' "strip" doesn't like:
+    #    strip: not enough room for program headers, try linking with -N
+    # As a workaround, strip manually before running patchelf.
+    strip -S $out/bin/alacritty
 
-      patchelf --set-rpath "${lib.makeLibraryPath rpathLibs}" $out/bin/alacritty
-    ''
-  ) + ''
+    patchelf --set-rpath "${lib.makeLibraryPath rpathLibs}" $out/bin/alacritty
+  '' + ''
 
     installShellCompletion --zsh extra/completions/_alacritty
 
